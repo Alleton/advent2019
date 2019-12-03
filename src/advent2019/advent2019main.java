35bce1f2cf68381239eb44b0574a8d438bcca4f0
alleton <jfalleton@gmail.com>
2019-12-03 16:04:06 +0100

    start 16.03 03 12

---
+++ src/advent2019/advent2019main.java
@@ -0,0 +1,19 @@
+package advent2019;
+/**
+ * Solve all advent 2019 code
+ */
+import model.AdventModel;
+
+
+public class advent2019main {
+	int problem ;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/controller/controller.java
@@ -0,0 +1,22 @@
+package controller;
+
+
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import model.AdventModel;;
+
+
+public class controller implements ActionListener{
+
+	AdventModel adventmodel ;
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
