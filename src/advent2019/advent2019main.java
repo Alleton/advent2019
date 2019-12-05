344bb1ccb2096d92411371b831e3f8840a5de8ae
alleton <jfalleton@gmail.com>
2019-12-05 11:24:09 +0100

    frame complete ?

--- src/advent2019/advent2019main.java
+++ src/advent2019/advent2019main.java
@@ -3,7 +3,8 @@ package advent2019;
  * Solve all advent 2019 code
  */
 import model.AdventModel;
-
+import view.AdventView ;
+import controller.AdventController;
 
 public class advent2019main {
 	int problem ;
@@ -13,6 +14,18 @@ public class advent2019main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		final int DAY_GRID = 5;
+		
+		/* **************** */
+		/* MVC              */
+		/* **************** */
+		AdventModel adventmodel = new  AdventModel();
+		AdventView adventprojectview = new view.AdventView() ;
+		AdventController adventprojectcontroller = new AdventController(adventmodel,adventprojectview );
+		
+		//addActionListener pour les boutons
+		adventprojectview.test.addActionListener(adventprojectcontroller);
+		adventprojectview.Go.addActionListener(adventprojectcontroller);
 
 	}
 
